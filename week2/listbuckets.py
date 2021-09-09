7400c4d1d72d3d052d79102205eb63c0e8c40f84
Tom Darligton <tdarlington@madisoncollege.edu>
2021-09-08 21:43:45 -0500

    not listing keys

---
+++ week2/listbuckets.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+import datetime
+
+def defaultconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+def to_json(self):
+    return json.dumps(self, default=defaultconverter, indent=4)
+
+def pretty_print(o):
+    print(to_json(o))
+
+client = boto3.client('s3')
+
+response = client.list_buckets()
+print("response")
+pretty_print(response)
+
+format_response = to_json(response)
+for bucket in response['Buckets']:
+    print('Bucket: '+bucket['Name'])
+    objects = client.list_objects_v2(Bucket=bucket['Name'])
+    objects = to_json(objects)
+    print("objects")
+    print(objects)
+    for content in objects['Contents']:
+        print(content['Key'])
+    
+    
