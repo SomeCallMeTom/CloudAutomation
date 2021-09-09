b8a8e6b90c2676debfb6d2b7220e2c6c14a77582
Tom Darligton <tdarlington@madisoncollege.edu>
2021-09-09 13:13:04 -0500

    json.loads to cast str to json object

--- week2/listbuckets.py
+++ week2/listbuckets.py
@@ -7,7 +7,7 @@ def defaultconverter(o):
         return o.__str__()
 
 def to_json(self):
-    return json.dumps(self, default=defaultconverter, indent=4)
+    return json.loads(json.dumps(self, default=defaultconverter, indent=4))
 
 def pretty_print(o):
     print(to_json(o))
@@ -15,17 +15,20 @@ def pretty_print(o):
 client = boto3.client('s3')
 
 response = client.list_buckets()
-print("response")
-pretty_print(response)
+# print("response")
+# pretty_print(response)
 
 format_response = to_json(response)
 for bucket in response['Buckets']:
     print('Bucket: '+bucket['Name'])
     objects = client.list_objects_v2(Bucket=bucket['Name'])
     objects = to_json(objects)
-    print("objects")
-    print(objects)
-    for content in objects['Contents']:
-        print(content['Key'])
-    
+    # print("objects")
+    # print(objects)
+    if "Contents" in objects:
+        # print (len(objects["Contents"]))
+        for content in objects['Contents']:
+            print("    "+content['Key'])
+    else:
+        print("    ! Bucket does not have content !")
     
