af79321044806cdd4e040293e0b338880bd4f1fc
Tom Darligton <tdarlington@madisoncollege.edu>
2021-09-18 15:03:01 -0500

    now working

--- week3/ec2.py
+++ week3/ec2.py
@@ -1,8 +1,5 @@
 import boto3
 
-
-
-
 #AMI = "ami-087c17d1fe0178315"
 def Get_Image(ec2_client):
 
@@ -28,6 +25,24 @@ def Create_EC2(AMI,ec2_client):
     )
     return response['Instances'][0]['InstanceId']
 
+def Print_Tags(ec2):
+    tags=ec2.tags
+    if tags is not None:
+        for tag in tags:
+            print(tag['Key']+" = "+tag['Value'])
+    else:
+        print("no tags found")
+def Create_Tag(ec2,key,value):
+    ec2.create_tags(
+        DryRun=DRYRUN,
+        Tags=[
+            {
+                'Key': key,
+                'Value': value
+            },
+        ]
+    )
+
 if __name__ == '__main__':
     DRYRUN = False
 
@@ -38,7 +53,12 @@ if __name__ == '__main__':
     ec2 = ec2_instance.Instance(instance_id)
     print(ec2.state['Name'])
     ec2.wait_until_running()
+    ec2 = ec2_instance.Instance(instance_id)
     print(ec2.state['Name'])
+    print(ec2.public_ip_address)
+    Print_Tags(ec2)
+    Create_Tag(ec2,"Name","Thomas")
+    Print_Tags(ec2)
     ec2.terminate()
     ec2.wait_until_terminated()
     print(ec2.state['Name'])
